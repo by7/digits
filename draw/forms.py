78897d8ca04e3993856a267f2bafe057659e11d0
Ben Yang <thisisbenbtw@gmail.com>
2017-10-15 23:15:39 -0700

    Implemented convolutional neural net
    bug: uses image on server startup rather than the image just submitted

--- draw/forms.py
+++ draw/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class ImageForm(forms.Form):
-    draw_image = forms.CharField(max_length=40000)
+    draw_image = forms.CharField(max_length=2000)
--- draw/views.py
+++ draw/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponseRedirect
 import re
 import base64
 
+import tensorflow as tf
+import numpy as np
+
 from .forms import ImageForm
 
 #def index(request):
 #    return render(request, 'draw/index.html')
 
 def index(request):
+    y_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -28,11 +32,40 @@ def index(request):
             # Decode the 64 bit string into 32 bit and save
             with open("imageToSave.png", "wb") as fh:
                 fh.write(base64.b64decode(ImageData))
+            y_list = predict_number()[0].tolist()
+            print(y_list)
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            #return HttpResponseRedirect('/thanks/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = ImageForm()
 
-    return render(request, 'draw/index.html', {'form': form})
+    return render(request, 'draw/index.html', {'form': form, 'y_list': y_list})
+
+def predict_number():
+    sess=tf.Session()    
+    #First let's load meta graph and restore weights
+    saver = tf.train.import_meta_graph('deep_model/deep_model.meta')
+    saver.restore(sess,tf.train.latest_checkpoint('./deep_model/'))
+
+    # Now, let's access and create placeholders variables and
+    # create feed-dict to feed new data
+
+    graph = tf.get_default_graph()
+    x = graph.get_tensor_by_name("x:0")
+    keep_prob = graph.get_tensor_by_name("dropout/keep_prob:0")
+    image = tf.image.decode_png(tf.read_file('imageToSave.png'), channels=1)
+    #image = tf.transpose(image, [1, 0, 2])
+    image = tf.image.rgb_to_grayscale(tf.image.resize_images(image,[28,28]))
+    image = tf.reshape(image, [1, 784]).eval(session=sess)
+    image = 1-(image/255)
+    feed_dict ={x: image, keep_prob: 1.0}
+
+    #Now, access the op that you want to run. 
+    y_conv = graph.get_tensor_by_name("fc2/y_conv:0")
+
+    probs = sess.run(tf.nn.softmax(y_conv),feed_dict)
+    np.set_printoptions(suppress=True)
+    print(probs)
+    return probs
